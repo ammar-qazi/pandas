da4811f942eb032ae06efb4efd93ac01c8a43e8e
Ammar Qazi <ammmarqz@gmail.com>
2024-09-08 05:11:32 +0200

    Remove Extensionarray.interpolate from code_checks.sh

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -122,7 +122,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Timestamp.resolution PR02" \
         -i "pandas.Timestamp.tzinfo GL08" \
         -i "pandas.Timestamp.year GL08" \
-        -i "pandas.api.extensions.ExtensionArray.interpolate PR01,SA01" \
         -i "pandas.api.types.is_bool PR01,SA01" \
         -i "pandas.api.types.is_categorical_dtype SA01" \
         -i "pandas.api.types.is_complex PR01,SA01" \
